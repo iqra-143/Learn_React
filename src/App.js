77ac374102be6c04fc9f00195052b096b4ca2dbb
iqra-143 <iqra75287@gmail.com>
2024-08-14 15:29:42 +0500

    practice hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+ 
+ 
+
+  const [counter,setCounter] = useState()
+  const [name, setName] = useState('Iqra');
+
+  // Function to change the name to 'Humna'
+  const changeName = () => {
+    setName('Humna');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+   <>
+    <h1>Text Area</h1>
+    <textarea value={counter} onChange={(e)=> {
+      console.log(e.target.value)
+      setCounter(e.target.value)}}>
+        
+    </textarea>
+    <div>
+      
+      {counter}
+
     </div>
-  );
+  <div>
+  <h1>Hello, my name is {name}</h1>
+  <button onClick={changeName}>Change Name</button>
+</div>
+   </>
+
+  )
 }
 
 export default App;
+
